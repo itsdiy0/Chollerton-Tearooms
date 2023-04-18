54a6844f92b71711ec3f5c7d289b06a95899e087
Mobin Rajaei <diyo11jen@gmail.com>
2023-04-18 14:04:17 +0100

    Add files via upload

---
+++ js/custom_slider.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// const changeSlideImage = function(src,n){
+//     let s = src.split("");
+//     s.splice(s.length-5,5);
+//     s.push(n.toString());
+//     s.push(".jpg");
+//     return s.join('');
+// };
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
