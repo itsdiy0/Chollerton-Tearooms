6ce1ebc3b48afe3492cf8c5963fc795340f82bfb
Mobin Rajaei <diyo11jen@gmail.com>
2023-04-21 13:31:47 +0100

    Update header.php

--- header.php
+++ header.php
@@ -5,15 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" type="image/x-icon" href="/assessts/images/CT_favIcon.png">
     <title>Chollerton Tearooms</title>
 </head>
 <body>
     <header class="main_top">
         <a href="index.php"><img src="assessts/images/CT_mainLogo.svg" alt=""></a>
         <nav class="main_top_nav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="findmore.php">Find out more</a></li>
-            <li><a href="#">Credits</a></li>
-            <li><a href="requests.php">View requests</a></li>
+            <menu>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="findmore.php">Find out more</a></li>
+                <li><a href="credits.php">Credits</a></li>
+                <li><a href="requests.php">View requests</a></li>
+            </menu>
         </nav>
     </header>
