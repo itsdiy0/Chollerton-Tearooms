d4c43efb75ba748afe885d2216a63c7eb830e553
Mobin Rajaei <diyo11jen@gmail.com>
2023-04-21 13:31:32 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <footer class="mainFooter">
+    <iframe class="locationMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36621.13315485302!2d-2.0795362463113087!3d54.993775435673655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487dc13abbaff969%3A0xc5d6e826f9261af6!2sChollerton%2C%20Hexham%20NE46%204TQ!5e0!3m2!1sen!2suk!4v1681728318143!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     <section class="footerDescription">
+        <h4>
+            Where are we ?
+        </h4>
         <div class="contactUS">
             Chollerton, Hexham NE46 4TQ
             <br>
@@ -9,18 +13,19 @@
         </div>
         <div class="socialSection">
             <h6>Find more about us ! </h6>
-            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>    
-            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>    
-            <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>    
+            <menu>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+            </menu>
         </div>
         <div class="copyRight">
             © 2023 Chollerton Tearooms. All rights reserved
             <br>
-            desgiend by diy0
+            desgiend by <a href="https://github.com/itsdiy0/Chollerton-Tearooms" target="_blank">diy0</a>
         </div>    
     </section>
-    <iframe class="locationMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36621.13315485302!2d-2.0795362463113087!3d54.993775435673655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487dc13abbaff969%3A0xc5d6e826f9261af6!2sChollerton%2C%20Hexham%20NE46%204TQ!5e0!3m2!1sen!2suk!4v1681728318143!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </footer>
 
 <script src="https://kit.fontawesome.com/69857d042c.js" crossorigin="anonymous"></script>
